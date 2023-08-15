faf262f456881992a085a6b1b55333eb66766b3c
Nick De Cooman <nickdecooman@users.noreply.github.com>
2023-08-15 10:46:04 +0200

    Make more changes

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__init__.py
@@ -1,5 +1,11 @@
 """**{{cookiecutter.project_name}}**
 
 {{cookiecutter.description}}
+
+Just adding some extra changes!
 """
 __version__ = "{{cookiecutter.version}}"
+
+def main():
+    """Main entry point of the app."""
+    print("Consider using the `poetry run` command.")
